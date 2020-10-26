b3f94e11721f416940d684de9ed2ea79c81263a9
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2020-10-26 17:17:30 +0000

    use more mock data in model tests (#14)

--- tests/unit/models/test_model_interfaces.py
+++ tests/unit/models/test_model_interfaces.py
@@ -98,8 +98,8 @@ def test_trainable_model_interface_set_optimize() -> None:
 
 def _mock_data() -> Tuple[tf.Tensor, tf.Tensor]:
     return (
-        tf.constant([[1.2]], gpflow.default_float()),
-        tf.constant([[3.4]], gpflow.default_float()),
+        tf.constant([[1.1], [2.2], [3.3], [4.4]], gpflow.default_float()),
+        tf.constant([[1.2], [3.4], [5.6], [7.8]], gpflow.default_float()),
     )
 
 
