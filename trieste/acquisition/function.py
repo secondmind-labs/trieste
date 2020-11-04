201f2da21eb239f1e9a0a1fbef8e3bfdd2079f89
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2020-11-04 16:06:08 +0000

    remove shape check

--- trieste/acquisition/function.py
+++ trieste/acquisition/function.py
@@ -124,13 +124,10 @@ def expected_improvement(model: ModelInterface, eta: tf.Tensor, at: QueryPoints)
        }
 
     :param model: The model of the objective function.
-    :param eta: The "best" observation, with shape [1].
+    :param eta: The "best" observation.
     :param at: The points for which to calculate the expected improvement.
     :return: The expected improvement at ``at``.
-    :raise ValueError (or InvalidArgumentError): If eta has the wrong shape.
     """
-    tf.debugging.assert_shapes([(eta, [1])], "eta has an invalid shape")
-
     mean, variance = model.predict(at)
     normal = tfp.distributions.Normal(mean, tf.sqrt(variance))
     return (eta - mean) * normal.cdf(eta) + variance * normal.prob(eta)
