c2111f6cf03bb40a29ffa8b5fef3c257fbc7c786
Joel Berkeley <joel.berkeley@secondmind.ai>
2020-11-16 00:01:54 +0000

    wip

--- tests/unit/test_observer.py
+++ tests/unit/test_observer.py
@@ -18,7 +18,7 @@ import tensorflow as tf
 from trieste.data import Dataset
 from trieste.observer import map_is_finite, filter_finite
 
-from tests.util.misc import ShapeLike, assert_dataset_allclose
+from tests.util.misc import ShapeLike, assert_datasets_allclose
 
 
 def _sum_with_nan_at_origin(t: tf.Tensor) -> tf.Tensor:
@@ -46,7 +46,7 @@ def _sum_with_nan_at_origin(t: tf.Tensor) -> tf.Tensor:
     (tf.zeros(0, 2), Dataset(tf.zeros(0, 2), tf.zeros(0, 1))),  # empty data
 ])
 def test_filter_finite(query_points: tf.Tensor, expected: Dataset) -> None:
-    assert_dataset_allclose(Dataset(query_points, _sum_with_nan_at_origin(query_points)), expected)
+    assert_datasets_allclose(Dataset(query_points, _sum_with_nan_at_origin(query_points)), expected)
 
 
 @pytest.mark.parametrize('qp_shape, obs_shape', [
@@ -64,9 +64,9 @@ def test_map_is_finite() -> None:
     query_points = tf.constant([[0., 0.]] + [[-1., 0.], [1., 0.], [0., 2.], [1., 3.]])
     is_finite = map_is_finite(query_points, _sum_with_nan_at_origin(query_points))
     expected = Dataset(query_points, tf.constant([[0.], [1.], [1.], [1.], [1.]]))
-    assert_dataset_allclose(is_finite, expected)
+    assert_datasets_allclose(is_finite, expected)
 
 
 def test_map_is_finite_with_empty_data() -> None:
     empty = tf.zeros(0, 2)
-    assert_dataset_allclose(map_is_finite(empty, empty), Dataset(empty, empty))
+    assert_datasets_allclose(map_is_finite(empty, empty), Dataset(empty, empty))
