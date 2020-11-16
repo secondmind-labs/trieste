259e543374e95d51d5f39749e68f1bb007ec0650
Joel Berkeley <joel.berkeley@secondmind.ai>
2020-11-16 14:26:57 +0000

    tidy imports

--- tests/unit/test_space.py
+++ tests/unit/test_space.py
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from _ast import operator
-from functools import reduce
 from typing import Tuple, List
 
 import pytest
