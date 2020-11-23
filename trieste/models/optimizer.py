c17bbf22ab8ca68b072a20600dd86b3602e5963c
Artem Artemev <art.art.v@gmail.com>
2020-11-23 23:15:56 +0000

    Address PR comments about types and renamings.

--- trieste/models/optimizer.py
+++ trieste/models/optimizer.py
@@ -3,15 +3,28 @@ from functools import singledispatch
 from dataclasses import dataclass, field
 import tensorflow as tf
 import gpflow
+import scipy
 
 from gpflow.models import ExternalDataTrainingLossMixin, InternalDataTrainingLossMixin
 from ..utils import jit
 from ..datasets import Dataset
 
 
-DatasetBuilder = Callable[[Dataset, Optional[int]], Union[Iterable, Tuple]]
+DatasetTransformer = Callable[[Dataset, Optional[int]], Union[Iterable, Tuple]]
 """
-Type alias for a function that creates minibatches from a :class:`~optiflow.datasets.Dataset`.
+Type alias for a function that maps a dataset from a :class:`~optiflow.datasets.Dataset`
+and generates an iterator over the transformation.
+"""
+
+LossClosure = Callable[[], tf.Tensor]
+"""
+Type alias for a loss closure that is used generally for optimizing.
+"""
+
+OptimizeResult = Union[scipy.optimize.OptimizeResult, None]
+"""
+Optimization result. For scipy optimizer it is :class:`~scipy.optimize.OptmizeResult`.
+TensorFlow opitimizer doesn't return any result.
 """
 
 
@@ -21,13 +34,13 @@ class Optimizer:
     minimize_args: Dict[str, Any] = field(default_factory=lambda: {})
     compile: bool = False
 
-    def create_loss(self, model: tf.Module, dataset: Dataset) -> Callable:
+    def create_loss(self, model: tf.Module, dataset: Dataset) -> LossClosure:
         x = tf.convert_to_tensor(dataset.query_points)
         y = tf.convert_to_tensor(dataset.observations)
         data = (x, y)
         return create_loss_function(model, data, self.compile)
 
-    def optimize(self, model: tf.Module, dataset: Dataset):
+    def optimize(self, model: tf.Module, dataset: Dataset) -> OptimizeResult:
         loss_fn = self.create_loss(model, dataset)
         variables = model.trainable_variables
         return self.optimizer.minimize(loss_fn, variables, **self.minimize_args)
@@ -37,10 +50,9 @@ class Optimizer:
 class TFOptimizer(Optimizer):
     max_iter: int = 100
     batch_size: Optional[int] = None
-    dataset_builder: Optional[DatasetBuilder] = None
+    dataset_builder: Optional[DatasetTransformer] = None
 
-
-    def create_loss(self, model: tf.Module, dataset: Dataset) -> Callable:
+    def create_loss(self, model: tf.Module, dataset: Dataset) -> LossClosure:
         def creator_fn(data: Union[Tuple, Iterable]):
             return create_loss_function(model, data, self.compile)
 
@@ -62,7 +74,7 @@ class TFOptimizer(Optimizer):
         data = self.dataset_builder(dataset, self.batch_size)
         return creator_fn(data)
 
-    def optimize(self, model: tf.Module, dataset: Dataset):
+    def optimize(self, model: tf.Module, dataset: Dataset) -> None:
         loss_fn = self.create_loss(model, dataset)
         variables = model.trainable_variables
 
@@ -81,14 +93,16 @@ def create_optimizer(optimizer, optimizer_args: Dict[str, Any]) -> Optimizer:
 
 @create_optimizer.register
 def _create_tf_optimizer(
-    optimizer: tf.optimizers.Optimizer, optimizer_args: Dict[str, Any],
+    optimizer: tf.optimizers.Optimizer,
+    optimizer_args: Dict[str, Any],
 ) -> Optimizer:
     return TFOptimizer(optimizer, **optimizer_args)
 
 
 @create_optimizer.register
 def _create_scipy_optimizer(
-    optimizer: gpflow.optimizers.Scipy, optimizer_args: Dict[str, Any],
+    optimizer: gpflow.optimizers.Scipy,
+    optimizer_args: Dict[str, Any],
 ) -> Optimizer:
     return Optimizer(optimizer, **optimizer_args)
 
@@ -97,19 +111,23 @@ Data = Union[Tuple[tf.Tensor, tf.Tensor], Iterable[Tuple[tf.Tensor, tf.Tensor]]]
 
 
 @singledispatch
-def create_loss_function(model, dataset: Data, compile: Optional[bool] = False) -> Callable:
+def create_loss_function(model, dataset: Data, compile: Optional[bool] = False) -> LossClosure:
     pass
 
 
 @create_loss_function.register
 def _create_loss_function_internal(
-    model: InternalDataTrainingLossMixin, data: Data, compile: Optional[bool] = False,
-) -> Callable:
+    model: InternalDataTrainingLossMixin,
+    data: Data,
+    compile: Optional[bool] = False,
+) -> LossClosure:
     return model.training_loss_closure(compile=compile)
 
 
 @create_loss_function.register
 def _create_loss_function_external(
-    model: ExternalDataTrainingLossMixin, data: Data, compile: Optional[bool] = False,
-) -> Callable:
+    model: ExternalDataTrainingLossMixin,
+    data: Data,
+    compile: Optional[bool] = False,
+) -> LossClosure:
     return model.training_loss_closure(data, compile=compile)
