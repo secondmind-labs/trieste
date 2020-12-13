20a56db83b0c0c86875c0c77b33126deecebae39
Joel Berkeley <joel.berkeley@secondmind.ai>
2020-12-13 02:16:00 +0000

    remove map_values

--- trieste/utils/misc.py
+++ trieste/utils/misc.py
@@ -55,27 +55,3 @@ def to_numpy(t: TensorType) -> np.ndarray:
         return t.numpy()
 
     return t
-
-
-K = TypeVar("K")
-""" A unbound type variable. """
-
-U = TypeVar("U")
-""" A unbound type variable. """
-
-V = TypeVar("V")
-""" A unbound type variable. """
-
-
-def map_values(f: Callable[[U], V], mapping: Mapping[K, U]) -> Mapping[K, V]:
-    """
-    Return the result of applying ``f`` to every value in ``m``. For example:
-
-    >>> map_values(lambda x: -x, {'a': 1, 'b': 2})
-    {'a': -1, 'b': -2}
-
-    :param f: A function to apply to each value in ``m``.
-    :param mapping: A mapping type to convert with ``f``.
-    :return: The result of applying ``f`` to every value in ``m``
-    """
-    return {k: f(u) for k, u in mapping.items()}
