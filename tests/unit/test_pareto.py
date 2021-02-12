2d77d6f672033d3f53eac1bfcfc41a6d4eb012ff
satrialoka <satrialoka@gmail.com>
2021-02-12 22:05:25 +0100

    change scores type from Dataset to tf.Tensor
    also remove Dataset import and fix non_dominated import

--- tests/unit/test_pareto.py
+++ tests/unit/test_pareto.py
@@ -16,28 +16,24 @@ import numpy.testing as npt
 import pytest
 import tensorflow as tf
 
-from trieste.data import Dataset
-from trieste.utils.pareto import non_dominated_sort
+from trieste.utils.pareto import non_dominated
 
 
 @pytest.mark.parametrize(
     "scores, pareto_set, dominance",
     [
         (
-            Dataset(
-                tf.ones([8, 2]),
-                tf.constant(
-                    [
-                        [0.9575, 0.4218],
-                        [0.9649, 0.9157],
-                        [0.1576, 0.7922],
-                        [0.9706, 0.9595],
-                        [0.9572, 0.6557],
-                        [0.4854, 0.0357],
-                        [0.8003, 0.8491],
-                        [0.1419, 0.9340],
-                    ]
-                ),
+            tf.constant(
+                [
+                    [0.9575, 0.4218],
+                    [0.9649, 0.9157],
+                    [0.1576, 0.7922],
+                    [0.9706, 0.9595],
+                    [0.9572, 0.6557],
+                    [0.4854, 0.0357],
+                    [0.8003, 0.8491],
+                    [0.1419, 0.9340],
+                ]
             ),
             tf.constant([[0.1576, 0.7922], [0.4854, 0.0357], [0.1419, 0.934]]),
             tf.constant([1, 5, 0, 7, 1, 0, 2, 0]),
@@ -45,6 +41,6 @@ from trieste.utils.pareto import non_dominated_sort
     ],
 )
 def test_dominated_sort(scores: tf.Tensor, pareto_set: tf.Tensor, dominance: tf.Tensor) -> None:
-    ret_pareto_set, ret_dominance = non_dominated_sort(scores.observations)
+    ret_pareto_set, ret_dominance = non_dominated(scores)
     npt.assert_allclose(ret_pareto_set, pareto_set)
     npt.assert_allclose(ret_dominance, dominance)
