48d8add7914be1f8df78585c16d135e4a3504f5f
satrialoka <satrialoka@gmail.com>
2021-02-20 00:05:25 +0100

    add duplicate points warning

--- trieste/utils/pareto.py
+++ trieste/utils/pareto.py
@@ -19,12 +19,16 @@ from trieste.type import TensorType
 
 def non_dominated(observations: TensorType) -> TensorType:
     """
-    Computes the non-dominated set for a set of data points
+    Computes the non-dominated set for a set of data points.
+    if there are duplicate point(s) in the non-dominated set, this function will return
+    as it is without removing the duplicate.
 
     :param observations: set of points with shape [N,D]
     :return: tf.Tensor of the non-dominated set [P,D] and the degree of dominance [N],
         P is the number of points in pareto front
         dominances gives the number of dominating points for each data point
+
+
     """
     extended = tf.tile(observations[None], [len(observations), 1, 1])
     swapped_ext = tf.transpose(extended, [1, 0, 2])
