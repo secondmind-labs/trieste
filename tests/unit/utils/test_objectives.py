d4bdc196016032ea21ec1f8ea3648621504279c6
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2021-02-22 15:37:46 +0000

    merge tests

--- tests/unit/utils/test_objectives.py
+++ tests/unit/utils/test_objectives.py
@@ -11,7 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from typing import Callable
+
 import numpy.testing as npt
+import pytest
 import tensorflow as tf
 
 from trieste.type import TensorType
@@ -35,12 +38,23 @@ def _unit_grid_2d() -> TensorType:
     return tf.squeeze(tf.stack([x0, x1], axis=-1))
 
 
+@pytest.mark.parametrize("objective, minimizers, minimum", [
+    (branin, BRANIN_MINIMIZERS, BRANIN_MINIMUM),
+    (gramacy_lee, GRAMACY_LEE_MINIMIZER, GRAMACY_LEE_MINIMUM),
+    (
+        logarithmic_goldstein_price,
+        LOGARITHMIC_GOLDSTEIN_PRICE_MINIMIZERS,
+        LOGARITHMIC_GOLDSTEIN_PRICE_MINIMUM
+    ),
+])
+def test_objective_maps_minimizers_to_minimum(
+    objective: Callable[[TensorType], TensorType], minimizers: TensorType, minimum: TensorType
+) -> None:
+    npt.assert_allclose(objective(minimizers), tf.squeeze(minimum))
+
+
 def test_branin_no_function_values_are_less_than_global_minimum() -> None:
     npt.assert_array_less(tf.squeeze(BRANIN_MINIMUM) - 1e-6, branin(_unit_grid_2d()))
-
-
-def test_branin_maps_argmin_values_to_global_minima() -> None:
-    npt.assert_allclose(branin(BRANIN_MINIMIZERS), tf.squeeze(BRANIN_MINIMUM), atol=1e-6)
 
 
 def test_gramacy_lee_no_points_are_less_than_global_minimum() -> None:
@@ -48,21 +62,11 @@ def test_gramacy_lee_no_points_are_less_than_global_minimum() -> None:
     npt.assert_array_less(tf.squeeze(GRAMACY_LEE_MINIMUM) - 1e-6, gramacy_lee(xs))
 
 
-def test_gramacy_lee_maps_argmin_values_to_global_minima() -> None:
-    function_values = gramacy_lee(GRAMACY_LEE_MINIMIZER)
-    npt.assert_allclose(function_values, tf.squeeze(GRAMACY_LEE_MINIMUM), atol=1e-6)
-
-
 def test_logarithmic_goldstein_price_no_function_values_are_less_than_global_minimum() -> None:
     npt.assert_array_less(
         tf.squeeze(LOGARITHMIC_GOLDSTEIN_PRICE_MINIMUM) - 1e-6,
         logarithmic_goldstein_price(_unit_grid_2d())
     )
-
-
-def test_logarithmic_goldstein_price_maps_argmin_values_to_global_minimum() -> None:
-    function_values = logarithmic_goldstein_price(LOGARITHMIC_GOLDSTEIN_PRICE_MINIMIZERS)
-    npt.assert_allclose(function_values, tf.squeeze(LOGARITHMIC_GOLDSTEIN_PRICE_MINIMUM), atol=1e-6)
 
 
 def test_mk_observer() -> None:
