0c72018b50b99df0e237f9eecef6bf06a3b7592c
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2021-03-10 17:12:02 +0000

    fix Cholesky error in notebook

--- docs/notebooks/inequality_constraints.pct.py
+++ docs/notebooks/inequality_constraints.pct.py
@@ -89,7 +89,8 @@ def create_bo_model(data):
     variance = tf.math.reduce_variance(initial_data[OBJECTIVE].observations)
     lengthscale = 1.0 * np.ones(2, dtype=gpflow.default_float())
     kernel = gpflow.kernels.Matern52(variance=variance, lengthscales=lengthscale)
-    gpr = gpflow.models.GPR(data.astuple(), kernel, noise_variance=1e-5)
+    jitter = gpflow.kernels.White(1e-12)
+    gpr = gpflow.models.GPR(data.astuple(), kernel + jitter, noise_variance=1e-5)
     gpflow.set_trainable(gpr.likelihood, False)
     return trieste.models.create_model({
         "model": gpr,
