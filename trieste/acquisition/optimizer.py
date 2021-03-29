f66e5698a6b732dfc0028c0f28df5da4fcc2de62
henrymoss <henry.moss@secondmind.ai>
2021-03-29 17:59:39 +0100

    sorted

--- trieste/acquisition/optimizer.py
+++ trieste/acquisition/optimizer.py
@@ -43,105 +43,102 @@ shape [..., B, D] output shape [..., 1], the :const:`AcquisitionOptimizer` retur
 
 @singledispatch
 def automatic_optimizer_selector(
-	space: SearchSpace, target_func: AcquisitionFunction
+    space: SearchSpace, target_func: AcquisitionFunction
 ) -> TensorType:
-	"""
-	A wrapper around our :const:`AcquisitionOptimizer`s. This class performs
-	an :const:`AcquisitionOptimizer` appropriate for the
-	problem's :class:`~trieste.space.SearchSpace`.
+    """
+    A wrapper around our :const:`AcquisitionOptimizer`s. This class performs
+    an :const:`AcquisitionOptimizer` appropriate for the
+    problem's :class:`~trieste.space.SearchSpace`.
+    :param space: The space of points over which to search, for points with shape [D].
+    :param target_func: The function to maximise, with input shape [..., 1, D] and output shape
+        [..., 1].
+    :return: The batch of points in ``space`` that maximises ``target_func``, with shape [1, D].
+    """
+    raise NotImplementedError(
+        """ No optimizer currentely supports acquisition function
+                maximisation over this search space"""
+    )
 
-	:param space: The space of points over which to search, for points with shape [D].
-	:param target_func: The function to maximise, with input shape [..., 1, D] and output shape
-		[..., 1].
-	:return: The batch of points in ``space`` that maximises ``target_func``, with shape [1, D].
-	"""
-	raise NotImplementedError(
-		""" No optimizer currentely supports acquisition function
-				maximisation over this search space"""
-		)
 
 @automatic_optimizer_selector.register
 def _discrete_space(space: DiscreteSearchSpace, target_func: AcquisitionFunction) -> TensorType:
-	return optimize_discrete(space,target_func)
+    return optimize_discrete(space, target_func)
 
 
 @automatic_optimizer_selector.register
 def _continuous_space(space: Box, target_func: AcquisitionFunction) -> TensorType:
-	return optimize_continuous(space,target_func)
+    return optimize_continuous(space, target_func)
 
 
 def batchify(
-	batch_size_one_optimizer: AcquisitionOptimizer[SP],
-	batch_size: int,
+    batch_size_one_optimizer: AcquisitionOptimizer[SP],
+    batch_size: int,
 ) -> AcquisitionOptimizer[SP]:
-	"""
-	A wrapper around our :const:`AcquisitionOptimizer`s. This class wraps a
-	:const:`AcquisitionOptimizer` to allow it to optimize batch acquisition functions.
+    """
+    A wrapper around our :const:`AcquisitionOptimizer`s. This class wraps a
+    :const:`AcquisitionOptimizer` to allow it to optimize batch acquisition functions.
+    :param batch_size_one_optimizer: An optimizer that returns only batch size one, i.e. produces a
+        single point with shape [1, D].
+    :param batch_size: The number of points in the batch.
+    :return: An :const:`AcquisitionOptimizer` that will provide a batch of points with shape [B, D].
+    """
+    tf.debugging.assert_positive(batch_size)
 
-	:param batch_size_one_optimizer: An optimizer that returns only batch size one, i.e. produces a
-		single point with shape [1, D].
-	:param batch_size: The number of points in the batch.
-	:return: An :const:`AcquisitionOptimizer` that will provide a batch of points with shape [B, D].
-	"""
-	tf.debugging.assert_positive(batch_size)
+    def optimizer(search_space: SP, f: AcquisitionFunction) -> TensorType:
+        expanded_search_space = search_space ** batch_size  # points have shape [B * D]
 
-	def optimizer(search_space: SP, f: AcquisitionFunction) -> TensorType:
-		expanded_search_space = search_space ** batch_size  # points have shape [B * D]
+        def target_func_with_vectorized_inputs(
+            x: TensorType,
+        ) -> TensorType:  # [..., 1, B * D] -> [..., 1]
+            return f(tf.reshape(x, x.shape[:-2].as_list() + [batch_size, -1]))
 
-		def target_func_with_vectorized_inputs(
-			x: TensorType,
-		) -> TensorType:  # [..., 1, B * D] -> [..., 1]
-			return f(tf.reshape(x, x.shape[:-2].as_list() + [batch_size, -1]))
+        vectorized_points = batch_size_one_optimizer(  # [1, B * D]
+            expanded_search_space, target_func_with_vectorized_inputs
+        )
+        return tf.reshape(vectorized_points, [batch_size, -1])  # [B, D]
 
-		vectorized_points = batch_size_one_optimizer(  # [1, B * D]
-			expanded_search_space, target_func_with_vectorized_inputs
-		)
-		return tf.reshape(vectorized_points, [batch_size, -1])  # [B, D]
-
-	return optimizer
+    return optimizer
 
 
 def optimize_discrete(space: DiscreteSearchSpace, target_func: AcquisitionFunction) -> TensorType:
-	"""
-	An :const:`AcquisitionOptimizer` for :class:'DiscreteSearchSpace' spaces and
-	batches of size of 1.
-
-	:param space: The space of points over which to search, for points with shape [D].
-	:param target_func: The function to maximise, with input shape [..., 1, D] and output shape
-		[..., 1].
-	:return: The **one** point in ``space`` that maximises ``target_func``, with shape [1, D].
-	"""
-	target_func_values = target_func(space.points[:, None, :])
-	tf.debugging.assert_shapes(
-		[(target_func_values, ("_", 1))],
-		message=(
-			f"The result of function target_func has an invalid shape:"
-			f" {tf.shape(target_func_values)}."
-		),
-	)
-	max_value_idx = tf.argmax(target_func_values, axis=0)[0]
-	return space.points[max_value_idx : max_value_idx + 1]
+    """
+    An :const:`AcquisitionOptimizer` for :class:'DiscreteSearchSpace' spaces and
+    batches of size of 1.
+    :param space: The space of points over which to search, for points with shape [D].
+    :param target_func: The function to maximise, with input shape [..., 1, D] and output shape
+        [..., 1].
+    :return: The **one** point in ``space`` that maximises ``target_func``, with shape [1, D].
+    """
+    target_func_values = target_func(space.points[:, None, :])
+    tf.debugging.assert_shapes(
+        [(target_func_values, ("_", 1))],
+        message=(
+            f"The result of function target_func has an invalid shape:"
+            f" {tf.shape(target_func_values)}."
+        ),
+    )
+    max_value_idx = tf.argmax(target_func_values, axis=0)[0]
+    return space.points[max_value_idx : max_value_idx + 1]
 
 
 def optimize_continuous(space: Box, target_func: AcquisitionFunction) -> TensorType:
-	"""
-	An :const:`AcquisitionOptimizer` for :class:'Box' spaces and batches of size of 1.
+    """
+    An :const:`AcquisitionOptimizer` for :class:'Box' spaces and batches of size of 1.
+    :param space: The space of points over which to search, for points with shape [D].
+    :param target_func: The function to maximise, with input shape [..., 1, D] and output shape
+        [..., 1].
+    :return: The **one** point in ``space`` that maximises ``target_func``, with shape [1, D].
+    """
+    trial_search_space = space.discretize(tf.minimum(2000, 500 * tf.shape(space.lower)[-1]))
 
-	:param space: The space of points over which to search, for points with shape [D].
-	:param target_func: The function to maximise, with input shape [..., 1, D] and output shape
-		[..., 1].
-	:return: The **one** point in ``space`` that maximises ``target_func``, with shape [1, D].
-	"""
-	trial_search_space = space.discretize(tf.minimum(2000, 500 * tf.shape(space.lower)[-1]))
+    initial_point = optimize_discrete(trial_search_space, target_func)  # [1, D]
 
-	initial_point = optimize_discrete(trial_search_space, target_func)  # [1, D]
+    bijector = tfp.bijectors.Sigmoid(low=space.lower, high=space.upper)
+    variable = tf.Variable(bijector.inverse(initial_point))  # [1, D]
 
-	bijector = tfp.bijectors.Sigmoid(low=space.lower, high=space.upper)
-	variable = tf.Variable(bijector.inverse(initial_point))  # [1, D]
+    def _objective() -> TensorType:
+        return -target_func(bijector.forward(variable[:, None, :]))  # [1]
 
-	def _objective() -> TensorType:
-		return -target_func(bijector.forward(variable[:, None, :]))  # [1]
+    gpflow.optimizers.Scipy().minimize(_objective, (variable,))
 
-	gpflow.optimizers.Scipy().minimize(_objective, (variable,))
-
-	return bijector.forward(variable)  # [1, D]
+    return bijector.forward(variable)  # [1, D]
