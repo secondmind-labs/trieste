58d520239007aef253f2372628e91749f30811e5
henrymoss <henry.moss@secondmind.ai>
2021-05-19 15:52:52 +0100

    blah

--- trieste/acquisition/optimizer.py
+++ trieste/acquisition/optimizer.py
@@ -100,7 +100,7 @@ def optimize_continuous(space: Box, target_func: AcquisitionFunction) -> TensorT
             [..., 1].
     :return: The **one** point in ``space`` that maximises ``target_func``, with shape [1, D].
     """
-    num_samples = tf.minimum(2000, 100 * tf.shape(space.lower)[-1])
+    num_samples =  1000 * tf.shape(space.lower)[-1]
     trial_search_space = space.discretize(num_samples)
     initial_point = optimize_discrete(trial_search_space, target_func)  # [1, D]
 
