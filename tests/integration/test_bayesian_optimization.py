33120f9840d6b8533617f1374dbb92e4f330c6d4
Henry Moss <32096840+henrymoss@users.noreply.github.com>
2021-07-09 12:03:26 +0100

    BayesianOptimizer can fit initial model (#283)
    * First go
    
    * fixed integration
    
    * still borken
    
    * pass types

--- tests/integration/test_bayesian_optimization.py
+++ tests/integration/test_bayesian_optimization.py
@@ -48,10 +48,10 @@ from trieste.utils.objectives import (
 @pytest.mark.parametrize(
     "num_steps, acquisition_rule",
     [
-        (7, EfficientGlobalOptimization()),
-        (15, EfficientGlobalOptimization(AugmentedExpectedImprovement().using(OBJECTIVE))),
+        (20, EfficientGlobalOptimization()),
+        (25, EfficientGlobalOptimization(AugmentedExpectedImprovement().using(OBJECTIVE))),
         (
-            7,
+            15,
             EfficientGlobalOptimization(
                 MinValueEntropySearch(Box([0, 0], [1, 1]), num_fourier_features=1000).using(
                     OBJECTIVE
@@ -59,14 +59,14 @@ from trieste.utils.objectives import (
             ),
         ),
         (
-            6,
+            10,
             EfficientGlobalOptimization(
                 BatchMonteCarloExpectedImprovement(sample_size=500).using(OBJECTIVE),
-                num_query_points=2,
+                num_query_points=3,
             ),
         ),
         (
-            4,
+            10,
             EfficientGlobalOptimization(
                 LocalPenalizationAcquisitionFunction(
                     Box([0, 0], [1, 1]),
@@ -75,17 +75,17 @@ from trieste.utils.objectives import (
             ),
         ),
         (
-            5,
+            10,
             EfficientGlobalOptimization(
                 GIBBON(
                     Box([0, 0], [1, 1]),
                 ).using(OBJECTIVE),
-                num_query_points=3,
+                num_query_points=2,
             ),
         ),
-        (7, TrustRegion()),
-        (7, DiscreteThompsonSampling(500, 3)),
-        (8, DiscreteThompsonSampling(500, 3, num_fourier_features=1000)),
+        (15, TrustRegion()),
+        (10, DiscreteThompsonSampling(500, 3)),
+        (10, DiscreteThompsonSampling(500, 3, num_fourier_features=1000)),
     ],
 )
 def test_optimizer_finds_minima_of_the_scaled_branin_function(
@@ -107,7 +107,7 @@ def test_optimizer_finds_minima_of_the_scaled_branin_function(
         gpflow.utilities.set_trainable(gpr.likelihood, False)
         return GaussianProcessRegression(gpr)
 
-    initial_query_points = search_space.sample_sobol(5)
+    initial_query_points = search_space.sample(5)
     observer = mk_observer(scaled_branin)
     initial_data = observer(initial_query_points)
     model = build_model(initial_data)
