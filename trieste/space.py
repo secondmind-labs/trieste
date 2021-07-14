ae7e24fd951d190975eaef61320512f0515173d9
satrialoka <satrialoka@gmail.com>
2021-07-14 20:06:34 +0200

    wip

--- trieste/space.py
+++ trieste/space.py
@@ -420,6 +420,7 @@ class OrdinalSearchSpace(Box):
         """ the step sizes of the ordinal space"""
         return self._stepsizes
 
+    @overload
     def __mul__(self, other: OrdinalSearchSpace) -> OrdinalSearchSpace:
         r"""
         Return the Cartesian product of the two :class:`OrdinalSearchSpace`\ es (concatenating 
