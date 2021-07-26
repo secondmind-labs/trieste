a28ef3be495d3ec7dd79bc5b8e52c6c90b678ecd
FlyWill <tsingjixiang@yahoo.com>
2021-07-26 15:02:39 +0200

    mysterious performance difference fix

--- tests/integration/test_multi_objective_bayesian_optimization.py
+++ tests/integration/test_multi_objective_bayesian_optimization.py
@@ -55,7 +55,7 @@ from trieste.utils.pareto import Pareto, get_reference_point
             EfficientGlobalOptimization(
                 BatchMonteCarloExpectedHypervolumeImprovement().using(OBJECTIVE), num_query_points=4
             ),
-            -3.21,
+            -3.2095,
             id="qehvi_vlmop2_q_4",
         ),
     ],
