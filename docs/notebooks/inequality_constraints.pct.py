484f2debb01da55e5bc10662853e9df533b6f04a
Victor <victor@secondmind.ai>
2021-07-27 13:36:22 +0100

    wip

--- docs/notebooks/inequality_constraints.pct.py
+++ docs/notebooks/inequality_constraints.pct.py
@@ -1,3 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.9.1
+#   kernelspec:
+#     display_name: trieste
+#     language: python
+#     name: trieste
+# ---
+
 # %% [markdown]
 # # Inequality constraints
 
