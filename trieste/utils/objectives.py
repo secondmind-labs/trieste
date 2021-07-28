375473e387c3b32533f98a19f236438b1f220f36
hstojic <hrvoje.stojic@protonmail.com>
2021-07-28 02:47:04 +0100

    added eggholder and michalewicz objective functions

--- trieste/utils/objectives.py
+++ trieste/utils/objectives.py
@@ -329,6 +329,74 @@ float64.
 """
 
 
+def eggholder(x: TensorType) -> TensorType:
+    """
+    The Eggholder function over :math:`[-512, 512]^2`. This function is difficult to optimize,
+    because of the large number of local minima. See https://www.sfu.ca/~ssurjano/egg.html
+    for details.
+
+    :param x: The points at which to evaluate the function, with shape [..., 2].
+    :return: The function values at ``x``, with shape [..., 1].
+    :raise ValueError (or InvalidArgumentError): If ``x`` has an invalid shape.
+    """
+    tf.debugging.assert_shapes([(x, (..., 2))])
+
+    x1 = x[..., :1]
+    x2 = x[..., 1:]
+
+    a = 47
+    b = 2
+
+    return -(x2+a) * tf.sin(tf.math.sqrt(tf.abs(x2+x1/b+a))) -x1 * tf.sin(tf.math.sqrt(tf.abs(x1-(x2+a))))
+
+
+EGGHOLDER_MINIMIZER = tf.constant(
+    [512, 404.2319], tf.float64
+)
+"""
+The global minimizer of the :func:`eggholder` function over :math:`[-512, 512]^2`, with shape [1, 2]
+and dtype float64.
+"""
+
+EGGHOLDER_MINIMUM = tf.constant([-959.6407], tf.float64)
+""" The global minimum of the :func:`eggholder` function, with shape [1] and dtype float64. """
+
+
+def michalewicz(x: TensorType, d: int = 2, m: int = 10) -> TensorType:
+    """
+    The Michalewicz function over :math:`[0, \\pi]` for all i=1,...,d. Dimensionality is determined by the parameter ``d`` and it features steep ridges and drops. It has :math:`d!` local minima, and it is multimodal. The parameter ``m`` defines the steepness of they valleys and ridges; a larger ``m`` leads to a more difficult search. The recommended value of ``m`` is 10. See https://www.sfu.ca/~ssurjano/egg.html
+    for details.
+
+    :param x: The points at which to evaluate the function, with shape [..., d].
+    :return: The function values at ``x``, with shape [..., 1].
+    :raise ValueError (or InvalidArgumentError): If ``x`` has an invalid shape.
+    """
+    tf.debugging.assert_greater_equal(d, 1)
+    tf.debugging.assert_shapes([(x, (..., d))])
+
+    xi = tf.range(1, (d+1), delta = 1, dtype = tf.float64)*tf.pow(x, 2)
+    result = tf.reduce_sum(tf.sin(x)*tf.pow(tf.sin(xi/math.pi), 2*m), axis=1, keepdims=True)
+
+    return -result
+
+
+MICHALEWICZ_2_MINIMIZER = tf.constant([2.20, 1.57], tf.float64
+)
+"""
+The global minimizer of the :func:`michalewicz` function over :math:`[0, \\pi]^2`,
+with shape [1, 2] and dtype float64.
+"""
+
+MICHALEWICZ_2_MINIMUM = tf.constant([-1.8013], tf.float64)
+""" The global minimum of the :func:`michalewicz` function, with shape [1] and dtype float64. """
+
+MICHALEWICZ_5_MINIMUM = tf.constant([-4.687658], tf.float64)
+""" The global minimum of the :func:`michalewicz` function, with shape [1] and dtype float64. """
+
+MICHALEWICZ_10_MINIMUM = tf.constant([-9.66015], tf.float64)
+""" The global minimum of the :func:`michalewicz` function, with shape [1] and dtype float64. """
+
+
 def mk_observer(objective: Callable[[TensorType], TensorType], key: str) -> Observer:
     """
     :param objective: An objective function designed to be used with a single data set and model.
