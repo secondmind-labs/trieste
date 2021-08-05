fe99c3c1f7288825db40f77168bfe078fa25f59c
Sebastian Ober <sebastian.ober@sebastian-dsktp.piointernal.prowler.io>
2021-08-05 13:56:08 +0100

    Added SVGP in supported_models

--- trieste/models/model_interfaces.py
+++ trieste/models/model_interfaces.py
@@ -766,7 +766,7 @@ supported_models: dict[Any, Callable[[Any, Optimizer], TrainableProbabilisticMod
     GPR: GaussianProcessRegression,
     SGPR: GaussianProcessRegression,
     VGP: VariationalGaussianProcess,
-    SVGP: SparseVariational,
+    SVGP: SparseVariational
 }
 """
 A mapping of third-party model types to :class:`CustomTrainable` classes that wrap models of those
