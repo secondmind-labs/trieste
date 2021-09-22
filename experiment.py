cf3cb9bfa7fd093855f28e83183a765747f905f7
Sebastian Ober <sebastian.ober@secondmind.ai>
2021-09-22 14:55:45 +0100

    Switched to makefile

--- experiment.py
+++ experiment.py
@@ -1,8 +1,9 @@
 import os
-# os.environ["CUDA_VISIBLE_DEVICES"] = ""
-import jug
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
 import numpy as np
 import tensorflow as tf
+tf.config.threading.set_intra_op_parallelism_threads(1)
+tf.config.threading.set_inter_op_parallelism_threads(1)
 from trieste.objectives import (
     michalewicz_2,
     michalewicz_5,
@@ -39,18 +40,31 @@ from exp_utils import (
     test_ll_gp,
     test_ll_vanilla_dgp,
 )
+import argparse
 
 np.random.seed(1794)
 tf.random.set_seed(1794)
 tf.keras.backend.set_floatx("float64")
 
-runs = 10
+parser = argparse.ArgumentParser()
+parser.add_argument('output_filename', type=str, help='output filename', nargs='?', default='test')
+parser.add_argument('--function', type=str, help='objective function', nargs='?', default='michalewicz2')
+parser.add_argument('--model', type=str, help='model name', nargs='?', default='deepgp')
+parser.add_argument('--ln', type=bool, help='whether to learn noise variance', nargs='?', default=False)
+parser.add_argument('--rt', type=bool, help='whether to retrain', nargs='?', default=False)
+parser.add_argument('--run', type=int, help='run number', nargs='?', default=0)
+args = parser.parse_args()
 
+function_key = args.function
+model_key = args.model
+learn_noise = args.ln
+retrain = args.rt
+run = args.run
 
 function_dict = {
-    "michalewicz_2": [michalewicz_2, MICHALEWICZ_2_MINIMUM, MICHALEWICZ_2_SEARCH_SPACE],
-    "michalewicz_5": [michalewicz_5, MICHALEWICZ_5_MINIMUM, MICHALEWICZ_5_SEARCH_SPACE],
-    "michalewicz_10": [michalewicz_10, MICHALEWICZ_10_MINIMUM, MICHALEWICZ_10_SEARCH_SPACE],
+    "michalewicz2": [michalewicz_2, MICHALEWICZ_2_MINIMUM, MICHALEWICZ_2_SEARCH_SPACE],
+    "michalewicz5": [michalewicz_5, MICHALEWICZ_5_MINIMUM, MICHALEWICZ_5_SEARCH_SPACE],
+    "michalewicz10": [michalewicz_10, MICHALEWICZ_10_MINIMUM, MICHALEWICZ_10_SEARCH_SPACE],
     "hartmann": [hartmann_6, HARTMANN_6_MINIMUM, HARTMANN_6_SEARCH_SPACE],
     "ackley": [ackley_5, ACKLEY_5_MINIMUM, ACKLEY_5_SEARCH_SPACE],
     "rosenbrock": [rosenbrock_4, ROSENBROCK_4_MINIMUM, ROSENBROCK_4_SEARCH_SPACE],
@@ -58,100 +72,94 @@ function_dict = {
 }
 
 model_dict = {
-    "deep_gp": [build_vanilla_dgp_model, test_ll_vanilla_dgp, False, False],
-    "gi_dgp": [build_gi_dgp_model, test_ll_gi_dgp, False, False],
-    "gp": [build_gp_model, test_ll_gp, False, False],
-    "deep_gp_ln": [build_vanilla_dgp_model, test_ll_vanilla_dgp, True, False],
-    "gi_dgp_ln": [build_gi_dgp_model, test_ll_gi_dgp, True, False],
-    "gp_ln": [build_gp_model, test_ll_gp, True, False],
-    "deep_gp_rt": [build_vanilla_dgp_model, test_ll_vanilla_dgp, False, True],
-    "gi_dgp_rt": [build_gi_dgp_model, test_ll_gi_dgp, False, True],
-    "gp_rt": [build_gp_model, test_ll_gp, False, True],
-    "deep_gp_ln_rt": [build_vanilla_dgp_model, test_ll_vanilla_dgp, True, True],
-    "gi_dgp_ln_rt": [build_gi_dgp_model, test_ll_gi_dgp, True, True],
-    "gp_ln_rt": [build_gp_model, test_ll_gp, True, True]
+    "deepgp": [build_vanilla_dgp_model, test_ll_vanilla_dgp],
+    "gidgp": [build_gi_dgp_model, test_ll_gi_dgp],
+    "gp": [build_gp_model, test_ll_gp],
 }
 
+if not os.path.exists(os.path.join('results', function_key)):
+    os.makedirs(os.path.join('results', function_key))
 
-for function_key in function_dict:
-    if not os.path.exists(os.path.join('results', function_key)):
-        os.makedirs(os.path.join('results', function_key))
+pd.DataFrame({
+    'function': [function_key],
+    'model': [model_key],
+    'learn_noise': [learn_noise],
+    'retrain': [retrain],
+    'run': [run]
+}).to_csv(args.output_filename)
 
-    function = function_dict[function_key][0]
-    F_MINIMIZER = function_dict[function_key][1]
+function = function_dict[function_key][0]
+F_MINIMIZER = function_dict[function_key][1]
 
-    search_space = function_dict[function_key][2]
-    observer = mk_observer(function)
+search_space = function_dict[function_key][2]
+observer = mk_observer(function)
 
-    bo = trieste.bayesian_optimizer.BayesianOptimizer(observer, search_space)
-    acquisition_rule = DiscreteThompsonSampling(1000, 1)
+bo = trieste.bayesian_optimizer.BayesianOptimizer(observer, search_space)
+acquisition_rule = DiscreteThompsonSampling(1000, 1)
 
-    num_initial_points = 20
-    num_acquisitions = 480
-    num_evaluation_points = 1000
-    for run in range(runs):
+num_initial_points = 20
+num_acquisitions = 480
+num_evaluation_points = 1000
 
-        @jug.TaskGenerator
-        def run_bayes_opt(
-                model_key,
-                initial_data,
-                ll_evaluation_data,
-                search_space
-        ):
-            start_time = time.time()
 
-            builder = model_dict[model_key][0]
-            tester = model_dict[model_key][1]
-            learn_noise = model_dict[model_key][2]
-            retrain = model_dict[model_key][3]
+def run_bayes_opt(
+        model_key,
+        initial_data,
+        ll_evaluation_data,
+        search_space
+):
+    start_time = time.time()
 
-            # Run Bayes Opt
-            if retrain:
-                num_acq_per_loop = 10
-                num_loops = num_acquisitions // num_acq_per_loop
+    builder = model_dict[model_key][0]
+    tester = model_dict[model_key][1]
 
-                current_dataset = initial_data
+    # Run Bayes Opt
+    if retrain:
+        num_acq_per_loop = 10
+        num_loops = num_acquisitions // num_acq_per_loop
 
-                for loop in range(num_loops):
-                    model = builder(current_dataset, learn_noise=learn_noise,
-                                    search_space=search_space)
-                    result = bo.optimize(num_acq_per_loop, current_dataset, model,
-                                             acquisition_rule=acquisition_rule, track_state=False)
-                    current_dataset = result.try_get_final_dataset()
-            else:
-                model = builder(initial_data, learn_noise=learn_noise, search_space=search_space)
+        current_dataset = initial_data
 
-                result = bo.optimize(num_acquisitions, initial_data, model,
+        for loop in range(num_loops):
+            model = builder(current_dataset, learn_noise=learn_noise,
+                            search_space=search_space)
+            result = bo.optimize(num_acq_per_loop, current_dataset, model,
                                      acquisition_rule=acquisition_rule, track_state=False)
+            current_dataset = result.try_get_final_dataset()
+    else:
+        model = builder(initial_data, learn_noise=learn_noise, search_space=search_space)
 
-            # Get results
-            result_dataset = result.try_get_final_dataset()
-            result_model = result.try_get_final_model()
+        result = bo.optimize(num_acquisitions, initial_data, model,
+                             acquisition_rule=acquisition_rule, track_state=False)
 
-            result_query_points = result_dataset.query_points.numpy()
-            result_observations = result_dataset.observations.numpy()
+    # Get results
+    result_dataset = result.try_get_final_dataset()
+    result_model = result.try_get_final_model()
 
-            result_arg_min_idx = tf.squeeze(tf.argmin(result_observations, axis=0))
+    result_query_points = result_dataset.query_points.numpy()
+    result_observations = result_dataset.observations.numpy()
 
-            result_evaluation_ll = tester(ll_evaluation_data, result_model)
+    result_arg_min_idx = tf.squeeze(tf.argmin(result_observations, axis=0))
 
-            pd.DataFrame(result_query_points).to_csv(
-                'results/{}/{}_query_points_{}'.format(function_key, model_key, run))
-            pd.DataFrame(result_observations).to_csv(
-                'results/{}/{}_observations_{}'.format(function_key, model_key, run))
-            pd.DataFrame(result_evaluation_ll).to_csv(
-                'results/{}/{}_ood_ll_{}'.format(function_key, model_key, run))
+    result_evaluation_ll = tester(ll_evaluation_data, result_model)
 
-            print(f"{model_key} observation "
-                  f"{function_key} {run}: {result_observations[result_arg_min_idx, :]}")
-            print(f"{model_key} OOD LL: {result_evaluation_ll}")
-            print("Time: ", time.time() - start_time)
+    pd.DataFrame(result_query_points).to_csv(
+        'results/{}/{}_query_points_{}'.format(function_key, model_key, run))
+    pd.DataFrame(result_observations).to_csv(
+        'results/{}/{}_observations_{}'.format(function_key, model_key, run))
+    pd.DataFrame(result_evaluation_ll).to_csv(
+        'results/{}/{}_ood_ll_{}'.format(function_key, model_key, run))
 
-        ll_evaluation_points = search_space.sample_sobol(num_evaluation_points)
-        ll_evaluation_data = observer(ll_evaluation_points)
+    print(f"{model_key} observation "
+          f"{function_key} {run}: {result_observations[result_arg_min_idx, :]}")
+    print(f"{model_key} OOD LL: {result_evaluation_ll}")
+    print("Time: ", time.time() - start_time)
 
-        initial_query_points = search_space.sample_sobol(num_initial_points)
-        initial_data = observer(initial_query_points)
 
-        for model_key in model_dict:
-            run_bayes_opt(model_key, initial_data, ll_evaluation_data, search_space)
+ll_evaluation_points = search_space.sample_sobol(num_evaluation_points)
+ll_evaluation_data = observer(ll_evaluation_points)
+
+initial_query_points = search_space.sample_sobol(num_initial_points)
+initial_data = observer(initial_query_points)
+
+run_bayes_opt(model_key, initial_data, ll_evaluation_data, search_space)
---
+++ makefile
@@ -0,0 +1,26 @@
+base = exec
+
+# General functions
+a1 = $(word 1,$(subst _, , $(1)))
+a2 = $(word 2,$(subst _, , $(1)))
+a3 = $(word 3,$(subst _, , $(1)))
+a4 = $(word 4,$(subst _, , $(1)))
+a5 = $(word 5,$(subst _, , $(1)))
+a6 = $(word 6,$(subst _, , $(1)))
+a7 = $(word 7,$(subst _, , $(1)))
+a8 = $(word 8,$(subst _, , $(1)))
+
+function_list = michalewicz2_ michalewicz5_ michalewicz10_ hartmann_ ackley_ rosenbrock_ shekel_
+model_list = gp_ deepgp_ gidgp_ gp_
+learn_noise = True_ False_
+retrain = True_ False_
+run = 0 1 2 3 4 5 6 7 8 9
+
+f_m = $(foreach pre,$(function_list),$(addprefix $(pre),$(model_list)))
+f_m_l = $(foreach pre,$(f_m),$(addprefix $(pre),$(learn_noise)))
+f_m_l_r = $(foreach pre,$(f_m_l),$(addprefix $(pre),$(retrain)))
+f_m_l_r_run = $(foreach pre,$(f_m_l_r),$(addprefix $(pre),$(run)))
+
+results/%: experiment.py
+	$(base) python $< $@ --function $(call a1,$*) --model $(call a2,$*) --ln $(call a3,$*) --rt $(call a4,$*) --run $(call a5,$*)
+experiment: $(addprefix results/,$(f_m_l_r_run))
