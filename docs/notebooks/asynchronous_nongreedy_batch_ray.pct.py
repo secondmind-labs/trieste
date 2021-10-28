9a0892509626f41292e883c753dbe82126e993d1
henrymoss <henry.moss@secondmind.ai>
2021-10-28 11:21:30 +0100

    remove dodge metadata

--- docs/notebooks/asynchronous_nongreedy_batch_ray.pct.py
+++ docs/notebooks/asynchronous_nongreedy_batch_ray.pct.py
@@ -1,17 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.10.2
-#   kernelspec:
-#     display_name: trieste
-#     language: python
-#     name: trieste
-# ---
-
 # %% [markdown]
 # # Asynchronous batch Bayesian optimization
 #
