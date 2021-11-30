862db0c4dc56d08e0dcd4d29d876fea17d97b5b1
hstojic <hrvoje.stojic@protonmail.com>
2021-11-30 15:58:35 +0000

    added increase in accuracy test; modified criteria

--- tests/integration/test_active_learning.py
+++ tests/integration/test_active_learning.py
@@ -41,14 +41,14 @@ from trieste.types import TensorType
 @pytest.mark.parametrize(
     "num_steps, acquisition_rule",
     [
-        (80, EfficientGlobalOptimization(PredictiveVariance())),
+        (50, EfficientGlobalOptimization(PredictiveVariance())),
     ],
 )
 def test_optimizer_learns_scaled_branin_function(
     num_steps: int, acquisition_rule: AcquisitionRule[TensorType, SearchSpace]
 ) -> None:
     """
-    Ensure that the objective function is effecitvely learned, such that the final model
+    Ensure that the objective function is effectively learned, such that the final model
     fits well and predictions are close to actual objective values.
     """
 
@@ -69,40 +69,57 @@ def test_optimizer_learns_scaled_branin_function(
 
         return GaussianProcessRegression(gpr)
 
-    num_initial_points = 20
+    num_initial_points = 6
     initial_query_points = search_space.sample_halton(num_initial_points)
     observer = mk_observer(scaled_branin)
     initial_data = observer(initial_query_points)
 
+    # we set a performance criterion at 1% of the range
+    # max absolute error needs to be bettter than this criterion
+    test_query_points = search_space.sample_sobol(10000 * search_space.dimension)
+    test_data = observer(test_query_points)
+    test_range = (tf.reduce_max(test_data.observations) - tf.reduce_min(test_data.observations))
+    criterion = 0.01 * test_range
+
+    # we expect a model with initial data to fail the criterion
+    initial_model = build_model(initial_data)
+    initial_model.optimize(initial_data)
+    initial_predicted_means, _ = initial_model.model.predict_f(test_query_points)  # type: ignore
+    initial_accuracy = tf.reduce_max(tf.abs(initial_predicted_means - test_data.observations))
+
+    assert not initial_accuracy < criterion
+
+    # after active learning the model should be much more accurate
     model = build_model(initial_data)
     final_model = (
         BayesianOptimizer(observer, search_space)
         .optimize(num_steps, initial_data, model, acquisition_rule)
         .try_get_final_model()
     )
+    final_predicted_means, _ = final_model.model.predict_f(test_query_points)  # type: ignore
+    final_accuracy = tf.reduce_max(tf.abs(final_predicted_means - test_data.observations))
 
-    test_query_points = search_space.sample_sobol(10000 * search_space.dimension)
-    test_data = observer(test_query_points)
-    predicted_means, _ = final_model.model.predict_f(test_query_points)  # type: ignore
-
-    npt.assert_allclose(predicted_means, test_data.observations, rtol=0.5)
+    assert initial_accuracy < final_accuracy
+    assert final_accuracy < criterion
 
 
 @random_seed
+@pytest.mark.slow
 @pytest.mark.parametrize(
     "num_steps, acquisition_rule, threshold",
     [
-        (150, EfficientGlobalOptimization(ExpectedFeasibility(80, delta=1)), 80),
-        (150, EfficientGlobalOptimization(ExpectedFeasibility(80, delta=2)), 80),
-        (150, EfficientGlobalOptimization(ExpectedFeasibility(20, delta=1)), 20),
+        (50, EfficientGlobalOptimization(ExpectedFeasibility(80, delta=1)), 80),
+        (50, EfficientGlobalOptimization(ExpectedFeasibility(80, delta=2)), 80),
+        (50, EfficientGlobalOptimization(ExpectedFeasibility(20, delta=1)), 20),
     ],
 )
 def test_optimizer_learns_feasibility_set_of_thresholded_branin_function(
     num_steps: int, acquisition_rule: AcquisitionRule[TensorType, SearchSpace], threshold: int
 ) -> None:
     """
-    Ensure that the objective function is effecitvely learned, such that the final model
-    fits well and predictions are close to actual objective values.
+    Ensure that the feasible set is sufficiently well learned, such that the final model
+    classifies with great degree of certainty whether points in the search space are in
+    in the feasible set or not.
     """
 
     search_space = BRANIN_SEARCH_SPACE
@@ -122,23 +139,36 @@ def test_optimizer_learns_feasibility_set_of_thresholded_branin_function(
 
         return GaussianProcessRegression(gpr)
 
-    num_initial_points = 20
+    num_initial_points = 6
     initial_query_points = search_space.sample_halton(num_initial_points)
     observer = mk_observer(branin)
     initial_data = observer(initial_query_points)
 
+    # we set a performance criterion at 0.001 probability of required precision per point
+    n_test = 10000 * search_space.dimension
+    test_query_points = search_space.sample_sobol(n_test)
+    criterion = 0.001 * (1 - 0.001) * tf.cast(n_test, tf.float64)
+
+    # we expect a model with initial data to fail the criterion
+    initial_model = build_model(initial_data)
+    initial_model.optimize(initial_data)
+    initial_prob = _excursion_probability(test_query_points, initial_model, threshold)
+    initial_accuracy = tf.reduce_sum(initial_prob * (1 - initial_prob))
+
+    assert not initial_accuracy < criterion
+
+    # after active learning the model should be much more accurate
     model = build_model(initial_data)
     final_model = (
         BayesianOptimizer(observer, search_space)
         .optimize(num_steps, initial_data, model, acquisition_rule)
         .try_get_final_model()
     )
+    final_prob = _excursion_probability(threshold_test_points, final_model, threshold)
+    final_accuracy = tf.reduce_sum(final_prob * (1 - final_prob))
 
-    test_points = search_space.sample_sobol(10000 * search_space.dimension)
-    prob = _excursion_probability(test_points, final_model, threshold)
-    accuracy = tf.reduce_sum(prob * (1 - prob))
-
-    assert accuracy < 0.0001
+    assert initial_accuracy < final_accuracy
+    assert final_accuracy < criterion
 
 
 def _excursion_probability(
