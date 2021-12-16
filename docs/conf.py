8fcb1c0b493480134873653ae5f9392810bb92f0
Uri Granta <uri.granta@secondmind.io>
2021-12-16 10:07:15 +0000

    Bump version to 0.9.0

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ copyright = (
 author = "The Trieste Contributors"
 
 # The full version, including alpha/beta/rc tags
-release = "0.8.0"
+release = "0.9.0"
 
 # -- General configuration ---------------------------------------------------
 
