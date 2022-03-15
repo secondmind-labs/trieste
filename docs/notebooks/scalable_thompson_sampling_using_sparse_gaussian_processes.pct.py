88119b357bd9bee536772a37d0c9699341a56e1c
uri-granta <50578464+uri-granta@users.noreply.github.com>
2022-03-15 11:10:01 +0000

    Fix slowtests broken by gpflow posterior pr (#532)

--- docs/notebooks/scalable_thompson_sampling_using_sparse_gaussian_processes.pct.py
+++ docs/notebooks/scalable_thompson_sampling_using_sparse_gaussian_processes.pct.py
@@ -24,12 +24,21 @@ tf.random.set_seed(1793)
 
 # %%
 import trieste
-from trieste.objectives import hartmann_6, HARTMANN_6_MINIMUM, HARTMANN_6_SEARCH_SPACE
+from trieste.objectives import (
+    hartmann_6,
+    HARTMANN_6_MINIMUM,
+    HARTMANN_6_SEARCH_SPACE,
+)
 from trieste.types import TensorType
+
 search_space = HARTMANN_6_SEARCH_SPACE
 
-def noisy_hartmann_6(x:TensorType) -> TensorType: # contaminate observations with Gaussian noise
-    return hartmann_6(x) + tf.random.normal([len(x),1], 0, 1, tf.float64) 
+
+def noisy_hartmann_6(
+    x: TensorType,
+) -> TensorType:  # contaminate observations with Gaussian noise
+    return hartmann_6(x) + tf.random.normal([len(x), 1], 0, 1, tf.float64)
+
 
 num_initial_data_points = 15
 initial_query_points = search_space.sample(num_initial_data_points)
@@ -40,23 +49,26 @@ initial_data = observer(initial_query_points)
 # We'll use a sparse Gaussian process regression to model the function, as implemented in GPflow. The GPflow models cannot be used directly in our Bayesian optimization routines, so we build a GPflow's `SVGP` model using Trieste's convenient model build function `build_svgp` and pass it to the `SparseVariational` wrapper. Note that we also define a `KMeansInducingPointSelector` selector, i.e. we reallocate the 50 inducing points of our `SVGP` model at the start of each BO step to be the centroids of a k-means clustering of the observations. As the optimization progresses, observations are likely to be concentrated in the optimal regions, so clustering provides “targeted” inducing points for BO.
 
 # %%
-from trieste.models.gpflow import SparseVariational, build_svgp, KMeansInducingPointSelector
+from trieste.models.gpflow import (
+    SparseVariational,
+    build_svgp,
+    KMeansInducingPointSelector,
+)
 from trieste.models.optimizer import BatchOptimizer
 
 gpflow_model = build_svgp(
-    initial_data, 
-    search_space, 
-    likelihood_variance=0.01, 
-    num_inducing_points=50
+    initial_data, search_space, likelihood_variance=0.01, num_inducing_points=50
 )
 
 inducing_point_selector = KMeansInducingPointSelector(search_space)
 
 model = SparseVariational(
-    gpflow_model, 
-    num_rff_features=1_000, 
+    gpflow_model,
+    num_rff_features=1_000,
     inducing_point_selector=inducing_point_selector,
-    optimizer = BatchOptimizer(tf.optimizers.Adam(0.1), max_iter=100, batch_size=50, compile=True)
+    optimizer=BatchOptimizer(
+        tf.optimizers.Adam(0.1), max_iter=100, batch_size=50, compile=True
+    ),
 )
 
 
@@ -64,7 +76,7 @@ model = SparseVariational(
 # ## Create the Thompson sampling acquisition rule
 #
 # Thompson sampling chooses query points as the minimizers of random samples from the model of our objective
-# function. 
+# function.
 #
 # Using a [decoupled sampling scheme](https://arxiv.org/abs/2002.09309), we can build approximate samples from our sparse GP surrogate model at low cost. As we can cheaply evaluate the values and gradients of these approximate samples at any point across the search space, our acquisition function optimizers can be used to find the minimizers of the samples across the whole search space. We can increase the quality of these approximate samples at the expense of computational cost by increasing `num_rff_features` (as specified when defining our model above).
 #
@@ -75,14 +87,15 @@ from trieste.acquisition.rule import EfficientGlobalOptimization
 from trieste.acquisition import ParallelContinuousThompsonSampling
 from trieste.acquisition.optimizer import automatic_optimizer_selector
 from trieste.acquisition.utils import split_acquisition_function_calls
+
 num_query_points = 100
 
-acq_rule =  EfficientGlobalOptimization(
-    builder = ParallelContinuousThompsonSampling(),
+acq_rule = EfficientGlobalOptimization(
+    builder=ParallelContinuousThompsonSampling(),
     num_query_points=num_query_points,
-    optimizer =  split_acquisition_function_calls(
+    optimizer=split_acquisition_function_calls(
         automatic_optimizer_selector, split_size=100_000
-    )
+    ),
 )
 
 # %% [markdown]
@@ -112,7 +125,9 @@ ground_truth_regret = hartmann_6(dataset.query_points) - HARTMANN_6_MINIMUM
 best_found_truth_idx = tf.squeeze(tf.argmin(ground_truth_regret, axis=0))
 
 fig, ax = plt.subplots()
-plot_regret(ground_truth_regret.numpy(),ax,num_init=10, idx_best=best_found_truth_idx)
+plot_regret(
+    ground_truth_regret.numpy(), ax, num_init=10, idx_best=best_found_truth_idx
+)
 
 ax.set_yscale("log")
 ax.set_ylabel("Regret")
