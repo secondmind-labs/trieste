22d305b7ee919de0e18f7edc1441bf752d2afd95
nloka <nasrulloh.loka@ugent.be>
2022-05-20 16:09:44 +0200

    add docstring

--- trieste/space.py
+++ trieste/space.py
@@ -179,6 +179,7 @@ class DiscreteSearchSpace(SearchSpace):
     def sample(self, num_samples: int, seed: Optional[int] = None) -> TensorType:
         """
         :param num_samples: The number of points to sample from this search space.
+        :param seed: Random seed for reproducibility
         :return: ``num_samples`` i.i.d. random points, sampled uniformly,
             from this search space.
         """
@@ -332,6 +333,7 @@ class Box(SearchSpace):
         Sample randomly from the space.
 
         :param num_samples: The number of points to sample from this search space.
+        :param seed: Random seed for reproducibility
         :return: ``num_samples`` i.i.d. random points, sampled uniformly,
             from this search space with shape '[num_samples, D]' , where D is the search space
             dimension.
