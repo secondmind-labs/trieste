c53ee8b77ffefa318941776657a995bf009a2a80
Uri Granta <uri.granta@secondmind.ai>
2022-07-11 13:19:29 +0100

    More typing

--- trieste/experimental/plotting/plotting_plotly.py
+++ trieste/experimental/plotting/plotting_plotly.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import annotations
+
 from typing import Callable, Optional
 
 import numpy as np
@@ -28,18 +30,19 @@ from .plotting import create_grid
 
 
 def format_point_markers(
-    num_pts,
-    num_init,
-    idx_best=None,
-    mask_fail=None,
-    m_init="x",
-    m_add="circle",
-    c_pass="green",
-    c_fail="red",
-    c_best="darkmagenta",
-):
+    num_pts: int,
+    num_init: int,
+    idx_best: Optional[int] = None,
+    mask_fail: Optional[TensorType] = None,
+    m_init: str = "x",
+    m_add: str = "circle",
+    c_pass: str = "green",
+    c_fail: str = "red",
+    c_best: str = "darkmagenta",
+) -> tuple[TensorType, TensorType]:
     """
     Prepares point marker styles according to some BO factors
+
     :param num_pts: total number of BO points
     :param num_init: initial number of BO points
     :param idx_best: index of the best BO point
@@ -63,9 +66,18 @@ def format_point_markers(
     return col_pts, mark_pts
 
 
-def add_surface_plotly(xx, yy, f, fig, alpha=1.0, figrow=1, figcol=1):
+def add_surface_plotly(
+    xx: TensorType,
+    yy: TensorType,
+    f: TensorType,
+    fig: go.Figure,
+    alpha: float = 1.0,
+    figrow: int = 1,
+    figcol: int = 1,
+) -> go.Figure:
     """
     Adds a surface to an existing plotly subfigure
+
     :param xx: [n, n] array (input)
     :param yy: [n, n] array (input)
     :param f: [n, n] array (output)
@@ -86,7 +98,17 @@ def add_surface_plotly(xx, yy, f, fig, alpha=1.0, figrow=1, figcol=1):
     return fig
 
 
-def add_bo_points_plotly(x, y, z, fig, num_init, idx_best=None, mask_fail=None, figrow=1, figcol=1):
+def add_bo_points_plotly(
+    x: TensorType,
+    y: TensorType,
+    z: TensorType,
+    fig: go.Figure,
+    num_init: int,
+    idx_best: Optional[int] = None,
+    mask_fail: Optional[TensorType] = None,
+    figrow: int = 1,
+    figcol: int = 1,
+) -> go.Figure:
     """
     Adds scatter points to an existing subfigure. Markers and colors are chosen according to
     BO factors.
@@ -186,11 +208,11 @@ def plot_function_plotly(
     mins: TensorType,
     maxs: TensorType,
     grid_density: int = 100,
-    title: str = None,
-    xlabel: str = None,
-    ylabel: str = None,
+    title: Optional[str] = None,
+    xlabel: Optional[str] = None,
+    ylabel: Optional[str] = None,
     alpha: float = 1.0,
-):
+) -> go.Figure:
     """
     Plots 2-dimensional plot of an objective function. To illustrate the function we create a
     regular grid of points and evaluate the function on these points.
