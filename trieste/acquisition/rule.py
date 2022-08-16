84903342effd3cb0cb5b01334f10c440fdfcd443
Alex Goodall <alexander.goodall@secondmind.ai>
2022-08-16 15:07:03 +0100

    change notebook to pct.py format and remove WIP test

--- trieste/acquisition/rule.py
+++ trieste/acquisition/rule.py
@@ -381,7 +381,8 @@ class AsynchronousRuleState:
 
         tf.debugging.assert_shapes(
             [(self.pending_points, [None, "D"]), (points_to_remove, [None, "D"])],
-            message=f"""Point to remove shall be 1xD where D is the last dimension of pending points.
+            message=f"""Point to remove shall be 1xD where D is the last dimension
+                        of pending points.
                         Got {tf.shape(self.pending_points)} for pending points
                         and {tf.shape(points_to_remove)} for other points.""",
         )
