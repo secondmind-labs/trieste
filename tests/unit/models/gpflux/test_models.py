8c01c5f464b6db0e30e354c89ab19ac2cb8df152
Hrvoje Stojic <hrvoje.stojic@protonmail.com>
2022-09-08 18:46:48 +0100

    gpflux log method (#620)
    * wip
    
    * draft
    
    * added tests
    
    * fixed likelihood logs
    
    * Update trieste/models/gpflux/models.py
    
    Co-authored-by: sebastianober <sebastian.w.ober@gmail.com>
    
    * Update trieste/models/gpflux/models.py
    
    Co-authored-by: sebastianober <sebastian.w.ober@gmail.com>
    
    * fixed docs
    
    Co-authored-by: sebastianober <sebastian.w.ober@gmail.com>

--- tests/unit/models/gpflux/test_models.py
+++ tests/unit/models/gpflux/test_models.py
@@ -26,6 +26,7 @@ from __future__ import annotations
 
 import copy
 import operator
+import unittest.mock
 from functools import partial
 from typing import Callable
 
@@ -43,6 +44,7 @@ from tests.util.misc import random_seed
 from tests.util.models.gpflux.models import single_layer_dgp_model
 from tests.util.models.models import fnc_2sin_x_over_3, fnc_3x_plus_10
 from trieste.data import Dataset
+from trieste.logging import step_number, tensorboard_writer
 from trieste.models.config import create_model
 from trieste.models.gpflux import DeepGaussianProcess
 from trieste.models.optimizer import KerasOptimizer
@@ -436,3 +438,70 @@ def test_deepgp_deep_copies_optimization_history() -> None:
     assert history.keys() == expected_history.keys()
     for k, v in expected_history.items():
         assert history[k] == v
+
+
+@unittest.mock.patch("trieste.logging.tf.summary.histogram")
+@unittest.mock.patch("trieste.logging.tf.summary.scalar")
+@pytest.mark.parametrize("use_dataset", [False, True])
+def test_deepgp_log(
+    mocked_summary_scalar: unittest.mock.MagicMock,
+    mocked_summary_histogram: unittest.mock.MagicMock,
+    use_dataset: bool,
+) -> None:
+    x_observed = np.linspace(0, 100, 100).reshape((-1, 1))
+    y_observed = fnc_2sin_x_over_3(x_observed)
+    dataset = Dataset(x_observed, y_observed)
+
+    model = DeepGaussianProcess(
+        single_layer_dgp_model(x_observed),
+        KerasOptimizer(tf.optimizers.Adam(), {"batch_size": 200, "epochs": 3, "verbose": 0}),
+    )
+    model.optimize(dataset)
+
+    mocked_summary_writer = unittest.mock.MagicMock()
+    with tensorboard_writer(mocked_summary_writer):
+        with step_number(42):
+            if use_dataset:
+                model.log(dataset)
+            else:
+                model.log(None)
+
+    assert len(mocked_summary_writer.method_calls) == 1
+    assert mocked_summary_writer.method_calls[0][0] == "as_default"
+    assert mocked_summary_writer.method_calls[0][-1]["step"] == 42
+
+    loss_names = ["loss/diff", "loss/final", "prior_kl/diff", "prior_kl/final"]
+    metrics_names = ["mse/diff", "mse/final"]
+    accuracy_names = [
+        "accuracy/root_mean_square_error",
+        "accuracy/mean_absolute_error",
+        "accuracy/z_residuals_std",
+    ]
+    variance_stats = [
+        "variance/predict_variance_mean",
+        "variance/empirical",
+        "variance/root_mean_variance_error",
+    ]
+    par_names = ["kernel.variance", "kernel.lengthscales", "likelihood"]
+
+    names_scalars = ["epochs/num_epochs"] + loss_names + metrics_names + par_names
+    num_scalars = len(names_scalars)
+    names_histogram = ["loss/epoch", "mse/epoch", "prior_kl/epoch"]
+    num_histogram = len(names_histogram)
+    if use_dataset:
+        names_scalars = names_scalars + accuracy_names + variance_stats
+        num_scalars = num_scalars + len(accuracy_names) + len(variance_stats)
+        names_histogram = (
+            names_histogram
+            + ["accuracy/absolute_errors", "accuracy/z_residuals"]
+            + ["variance/predict_variance", "variance/variance_error"]
+        )
+        num_histogram += 4
+
+    assert mocked_summary_scalar.call_count == num_scalars
+    for i in range(len(mocked_summary_scalar.call_args_list)):
+        assert any([j in mocked_summary_scalar.call_args_list[i][0][0] for j in names_scalars])
+
+    assert mocked_summary_histogram.call_count == num_histogram
+    for i in range(len(mocked_summary_histogram.call_args_list)):
+        assert any([j in mocked_summary_histogram.call_args_list[i][0][0] for j in names_histogram])
