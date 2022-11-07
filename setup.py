e0ebc18a645473c3ce4eff25f70bac43ae735266
Uri Granta <uri.granta@secondmind.ai>
2022-11-07 09:05:31 +0000

    Looks like dill 0.3.6 has an issue with ABCs on Python 3.7

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     python_requires="~=3.7",
     install_requires=[
         "absl-py",
-        "dill",
+        "dill!=0.3.6",
         "gpflow>=2.6.3",
         "gpflux>=0.2.3",
         "numpy",
