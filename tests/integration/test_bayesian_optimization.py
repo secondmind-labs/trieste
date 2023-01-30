ce14fa909c10b5729ed12262e08cd97da084eba6
uri-granta <50578464+uri-granta@users.noreply.github.com>
2023-01-30 17:56:24 +0000

    Workaround for alltests crash (#685)

--- tests/integration/test_bayesian_optimization.py
+++ tests/integration/test_bayesian_optimization.py
@@ -292,7 +292,7 @@ def test_bayesian_optimizer_with_svgp_finds_minima_of_scaled_branin() -> None:
     )
     _test_optimizer_finds_minimum(
         SparseVariational,
-        15,
+        25,
         EfficientGlobalOptimization[SearchSpace, SparseVariational](
             builder=ParallelContinuousThompsonSampling(), num_query_points=5
         ),
@@ -349,7 +349,7 @@ def test_bayesian_optimizer_with_sgpr_finds_minima_of_simple_quadratic() -> None
             25,
             EfficientGlobalOptimization(
                 ParallelContinuousThompsonSampling(),
-                num_query_points=3,
+                num_query_points=4,
             ),
             id="ParallelContinuousThompsonSampling",
         ),
