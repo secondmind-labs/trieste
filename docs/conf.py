c9272e23ffc05136dc3169e216e539fafa08bfea
Uri Granta <uri.granta@secondmind.ai>
2023-05-18 15:04:50 +0100

    Slightly deuglify

--- docs/conf.py
+++ docs/conf.py
@@ -115,6 +115,7 @@ html_theme = "pydata_sphinx_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
 
 # library logo location
 html_logo = "_static/logo.png"
