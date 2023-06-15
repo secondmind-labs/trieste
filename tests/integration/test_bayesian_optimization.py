7154e70aaa09c9d65ee7d9b8e44658dc60130bbb
Uri Granta <uri.granta@secondmind.ai>
2023-06-15 11:20:07 +0100

    Lazy variable creation results in one more compilation

--- tests/integration/test_bayesian_optimization.py
+++ tests/integration/test_bayesian_optimization.py
@@ -661,9 +661,10 @@ def _test_optimizer_finds_minimum(
                 assert acq_function is not None
 
                 # check that acquisition functions defined as classes aren't retraced unnecessarily
-                # they should be retraced for the optimzier's starting grid, L-BFGS, and logging
+                # they should be retraced for the optimizer's starting grid, L-BFGS, and logging
+                # (and possibly once more due to variable creation)
                 if isinstance(acq_function, (AcquisitionFunctionClass, TrajectoryFunctionClass)):
-                    assert acq_function.__call__._get_tracing_count() == 3  # type: ignore
+                    assert acq_function.__call__._get_tracing_count() in {3, 4}  # type: ignore
 
                 # update trajectory function if necessary, so we can test it
                 if isinstance(acq_function, TrajectoryFunctionClass):
