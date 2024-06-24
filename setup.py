ceeb10a13e4752e2891d635e5694c9efc43fe9f7
Uri Granta <uri.granta@secondmind.ai>
2024-06-24 12:26:40 +0100

    Try testing against Python 3.8 instead

--- setup.py
+++ setup.py
@@ -32,11 +32,11 @@ setup(
         "trieste": ["py.typed", "VERSION"],
     },
     classifiers=[
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires="~=3.7",
+    python_requires="~=3.8",
     install_requires=[
         "absl-py",
         "dill<0.3.6",
