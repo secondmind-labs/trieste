d1cb88fe44c4722f168fc87dc2e96d411120b860
Uri Granta <uri.granta@secondmind.ai>
2024-08-20 16:26:54 +0100

    Integration test and fix thompson sampling

--- trieste/models/interfaces.py
+++ trieste/models/interfaces.py
@@ -904,3 +904,10 @@ class EncodedFastUpdateModel(EncodedProbabilisticModel, FastUpdateModel):
         return self.conditional_predict_y_encoded(
             self.encode(query_points), self.encode(additional_data)
         )
+
+
+def get_encoder(model: ProbabilisticModel) -> EncoderFunction | None:
+    """Helper function for getting an encoder from model (which may or may not have one)."""
+    if isinstance(model, EncodedProbabilisticModel):
+        return model.encoder
+    return None
