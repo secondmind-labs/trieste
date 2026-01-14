2510cb72936dac00c96579b1f02184d3c6ff9c38
Chris Morter <chris@secondmind.ai>
2026-01-14 15:47:26 +0000

    .

--- trieste/space.py
+++ trieste/space.py
@@ -667,6 +667,7 @@ class CategoricalSearchSpace(GeneralDiscreteSearchSpace, HasOneHotEncoder):
         def encoder(x: TensorType) -> TensorType:
             flat_x, unflatten = flatten_leading_dims(x)
             tf.print(f"flat_x shape = ", tf.shape(flat_x))
+            tf.print(f"n_tags = ", n_tags)
             tf.debugging.assert_equal(tf.shape(flat_x)[-1], n_tags)
             columns = tf.split(flat_x, n_tags, axis=1)
             encoders = [
