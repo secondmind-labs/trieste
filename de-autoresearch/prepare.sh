8bee7669fe349e01286710c7796a14d08ee456af
alessandro.vullo <alessandro.vullo@secondmind.ai>
2026-05-18 11:06:31 +0100

    Commands to install and prepare the environment.

---
+++ de-autoresearch/prepare.sh
@@ -0,0 +1,8 @@
+python -m pip install --upgrade pip
+pip install -e .
+python3 -m pip install 'tensorflow[and-cuda]'
+pip install .[qhsri] -r tests/latest/requirements.txt -c tests/latest/constraints.txt
+
+export PATH="/usr/local/cuda/bin:$HOME/.local/bin:$PATH"
+export TF_FORCE_GPU_ALLOW_GROWTH=true
+export CUDA_DEVICE_ORDER=PCI_BUS_ID
