164a3b94e2c9d90ba89f93bc83b87729dcc072ea
uri-granta <50578464+uri-granta@users.noreply.github.com>
2026-05-22 17:40:11 +0100

    Upgrade to latest GPflow (#930)

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     install_requires=[
         "absl-py",
         "dill",
-        "gpflow>=2.9.2",
+        "gpflow>=2.10.1",
         "gpflux>=0.4.4",
         "numpy",
         "tensorflow>=2.5,<2.17; platform_system!='Darwin' or platform_machine!='arm64'",
